4a2d10aac99dc77db3c90ceb263a8373a98e2872
Leon Maughan <leonm@Leons-MacBook.utr.nl.meteogroup.net>
2022-02-24 14:15:30 +0100

    start to strip tutorial

--- plotlyflask_tutorial/__init__.py
+++ plotlyflask_tutorial/__init__.py
@@ -1,9 +1,9 @@
 """Initialize Flask app."""
-from ddtrace import patch_all
+#from ddtrace import patch_all
 from flask import Flask
 from flask_assets import Environment
 
-patch_all()
+#patch_all()
 
 
 def init_app():
